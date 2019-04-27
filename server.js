bfb226d85663b078e2d8f5a27d21f8a7fe620d2b
Kyanij Maharjan <mkyanij1@gmail.com>
2019-04-27 08:31:14 +0545

    necessary code changes (#1)
    * necessary code changes
    
    * Added db file
    
    * intent code

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-
+const db = require('./db');
 
 let Article = require('./models/article');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+// routes
 app.get('/articles',(req,res) => {
     Article.find(function(err, articlesList) {
         if(err) res.send({error: err.message})
@@ -23,11 +24,7 @@ app.post('/articles',(req,res) => {
         if(err) res.send({error: err.message})
         res.send(createData)
     })
-   
 });
-
-mongoose.connect('mongodb://localhost/news');
-let db = mongoose.connection;
 
 // Check for Db errors
 db.on('error', (err) => console.log(err))
@@ -37,4 +34,10 @@ db.once('open', () =>  {
     console.log("connected to mongodb")
     app.listen(3000,() => console.log("server running at 3000"));
 })
+
+
+
+
+
+
 
