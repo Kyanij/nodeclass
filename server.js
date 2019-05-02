b4a0f875abb1a99aa9292b982706ee3e0129d02e
Kyanij Maharjan <mkyanij1@gmail.com>
2019-05-02 08:15:04 +0545

    Added route in seperate route directory

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const route = require('./routes/route');
+
 const db = require('./db');
 
-let Article = require('./models/article');
-
 const app = express();
+
+app.use(route);
 
 // Pug Template
 app.set('views', './views')
@@ -13,26 +15,6 @@ app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 
-// routes
-app.get('/',(req,res) => {
-    res.render('index', {title:'Template', message:"hello there"});
-});
-
-app.get('/articles',(req,res) => {
-    Article.find(function(err, articlesList) {
-        if(err) res.send({error: err.message})
-        res.send(articlesList)
-    })
-});
-
-app.post('/articles',(req,res) => {
-    console.log(req.body);
-    const data = req.body;
-    Article.create(data, (err,createData)=> {
-        if(err) res.send({error: err.message})
-        res.send(createData)
-    })
-});
 
 // Check for Db errors
 db.on('error', (err) => console.log(err))
@@ -42,6 +24,7 @@ db.once('open', () =>  {
     console.log("connected to mongodb")
     app.listen(3000,() => console.log("server running at 3000"));
 })
+
 
 
 
